1fe94cb9d1fa3cd9cef7473183d59b33c498e149
Karim Baali <baali.karim95@gmail.com>
2018-03-23 10:49:25 +0100

    Handling all requests

--- public/index.js
+++ public/index.js
@@ -4,5 +4,39 @@ $('#query1').click(function(){
 	    console.log(res);
 	    $("#result_request_1").text(res)
 	})
-	
+})
+
+$('#query2').click(function(){
+	$.post("/query/2", (res) => {
+	    console.log(res);
+	    $("#result_request_2").text(res)
+	})
+})
+
+$('#query3').click(function(){
+	$.post("/query/3", (res) => {
+	    console.log(res);
+	    $("#result_request_3").text(res)
+	})
+})
+
+$('#query4').click(function(){
+	$.post("/query/4", (res) => {
+	    console.log(res);
+	    $("#result_request_4").text(res)
+	})
+})
+
+$('#query5').click(function(){
+	$.post("/query/5", (res) => {
+	    console.log(res);
+	    $("#result_request_5").text(res)
+	})
+})
+
+$('#query6').click(function(){
+	$.post("/query/6", (res) => {
+	    console.log(res);
+	    $("#result_request_6").text(res)
+	})
 })
